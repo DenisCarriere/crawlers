d4ac8c530a44e1cb57cd96719d57dc845b27a768
Denis <carriere.denis@gmail.com>
2014-08-12 16:50:10 -0400

    added stats

--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ import os
 
 
 # Program Variables
-provider = 'osm'
+provider = 'bing'
 city = 'ottawa'
 
 # Connec to MongoDB
@@ -14,5 +14,14 @@ db_city = db[city]
 db_geocoder = db[provider]
 
 
+# Finds
+ok = db_geocoder.find({'status':{'$ne':'OK'}})
+distance = db_geocoder.find({'distance':{'$gt':5000}, 'status':{'$ne':'ERROR - No results found'}})
 
-print db_city.count()
+print ok.count()
+print distance.count()
+
+"""
+for item in distance:
+	print item.get('location'), '                ------', item.get('address')
+"""
